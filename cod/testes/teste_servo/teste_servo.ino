8f622f6952fcb594650a0181cd61c22832c1f87f
Louzeiro <flalouzeiro@hotmail.com>
2023-04-08 16:03:29 -0300

    Teste servo

--- cod/testes/teste_servo/teste_servo.ino
+++ cod/testes/teste_servo/teste_servo.ino
@@ -2,28 +2,18 @@
 
 
 Servo myservo;  // create servo object to control a servo
-
-
-int potpin = 0;  // analog pin used to connect the potentiometer
-
-int val;    // variable to read the value from the analog pin
-
+int val;        // variable to read the value from the analog pin
+int timeServo = 100;  //
 
 void setup() {
-
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-
 }
 
 
 void loop() {
-
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-
-  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-
+  val = map(90, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
   myservo.write(val);                  // sets the servo position according to the scaled value
-
-  delay(15);                           // waits for the servo to get there
-
+  delay(timeServo);                           // waits for the servo to get there
+  val = map(0, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+  myservo.write(val);                  // sets the servo position according to the scaled value
 }

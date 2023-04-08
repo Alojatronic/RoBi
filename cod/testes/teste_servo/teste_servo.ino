942f41d35672abdd07dfde0a8eb394057edf6701
Louzeiro <flalouzeiro@hotmail.com>
2023-04-08 17:20:59 -0300

    adicionando teste sensor ultrassonico

--- cod/testes/teste_servo/teste_servo.ino
+++ cod/testes/teste_servo/teste_servo.ino
@@ -2,21 +2,18 @@
 
 
 Servo myservo;  // create servo object to control a servo
-int val;        // variable to read the value from the analog pin
-int timeServo = 100;  //
+int val1=90;        // variable to read the value from the analog pin
+int val2=0;        // variable to read the value from the analog pin
+
+int timeServo = 1000;  //
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
-
 void loop() {
-  
-  val = map(90, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-  myservo.write(val);                  // sets the servo position according to the scaled value
+  myservo.write(val1);                  // sets the servo position according to the scaled value
   delay(timeServo);                           // waits for the servo to get there
-  val = map(0, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-  myservo.write(val);                  // sets the servo position according to the scaled value
+  myservo.write(val2);                  // sets the servo position according to the scaled value
   delay(timeServo);                           // waits for the servo to get there
-  
 }
